9cc40b9babab11fc26a0af7c457d752abf7b5a22
Vincent Siu <vince.siu4@gmail.com>
2017-04-17 06:39:27 +0000

    Implement slave sending and master saving for connection data

--- master.py
+++ master.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3
 
+import pickle
 import pika
+
+
+
+
+# Change this function to write to a file we open
+def callback(ch, method, properties, pickle_dic):
+    print(" [x] Received something")
+
+    dic = pickle.loads(pickle_dic, fix_imports = True, encoding = 'ASCII', errors = 'strict')
+    write_data(dic)
+
+
+def write_data(dic):
+    file_object = open('log_' + dic['host_url'] + '.txt', 'a')
+    file_object.write(dic['target_url'] + ',' + dic['digest'] + ',' + dic['response_time'] + '\n')
+    file_object.close() 
+
 
 
 if __name__ == '__main__':
@@ -8,12 +26,6 @@ if __name__ == '__main__':
     channel = connection.channel()
 
     channel.queue_declare(queue='hello')
-
-
-
-# Change this function to write to a file we open
-    def callback(ch, method, properties, body):
-        print(" [x] Received %r" % body)
 
     channel.basic_consume(callback,
                           queue='hello',
--- slave.py
+++ slave.py
@@ -1,7 +1,41 @@
 #!/usr/bin/python3
 
+import hashlib
 import pika
+import time
+import requests
+import pickle
 
+from random import choice
+
+
+def get_info(input_url):
+
+    resp = requests.get(input_url, timeout = 0.25)
+    ret = {}
+
+    
+    if resp.status_code is not 200:
+        return None
+
+    m = hashlib.sha256()
+    m.update(resp.content)
+
+    ret['digest'] = m.hexdigest()
+    ret['host_url'] = choice(['192.168.0.1', '10.0.0.4', '333.524.1.8'])
+    ret['target_url'] = resp.url
+    ret['response_time'] = str(resp.elapsed)
+
+    resp.close()
+
+
+    
+    return ret
+
+
+#def get_info_range(ip_min, ip_max):
+#   for(
+    
 
 if __name__ == '__main__':
 
@@ -11,9 +45,22 @@ if __name__ == '__main__':
 
     channel.queue_declare(queue='hello')
 
+    test_dict = get_info('https://www.google.com')
+    test_ip = get_info('http://74.125.21.104')
 
+    pickled_dict = pickle.dumps(test_ip, pickle.HIGHEST_PROTOCOL)
+    
+    google_ips = ['74.125.21.147', '74.125.21.105', '74.125.21.103', '74.125.21.106', '74.125.21.99', '74.125.21.104']
+
+    
+
+    # Check if IP address is not black hole
     channel.basic_publish(exchange='',
                           routing_key='hello',
-                          body='Hello World!')
-    print(" [x] Sent 'Hello World!'")
+                          body=pickled_dict)
+
+
+    print(" [x] Sent message")
     connection.close()
+
+#print(repr(get_info('https://www.google.com')))
